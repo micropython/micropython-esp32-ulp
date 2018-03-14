cb4c7aecf3ad5c6d80b1f340524cff95093549db
Ivan Grokhotkov <ivan@espressif.com>
2018-03-14 11:28:43 +0800

    ci: clean up output, use xxd when dumping binaries

--- tests/01_compat_tests.sh
+++ tests/01_compat_tests.sh
@@ -4,35 +4,36 @@
 
 set -e
 
-for file in opcodes assemble link ; do
-    echo testing $file...
-    micropython $file.py
-done
-
 for src_file in $(ls -1 compat/*.S); do
     src_name="${src_file%.S}"
     
-    echo "Building $src_file using py-esp32-ulp"
+    echo "Testing $src_file"
+    echo -e "\tBuilding using py-esp32-ulp"
     ulp_file="${src_name}.ulp"
-    micropython -m esp32_ulp $src_file    # generates $ulp_file
+    log_file="${src_name}.log"
+    micropython -m esp32_ulp $src_file 1>$log_file   # generates $ulp_file
 
     obj_file="${src_name}.o"
     elf_file="${src_name}.elf"
     bin_file="${src_name}.bin"
 
-    echo "Building $src_file using binutils"
+    echo -e "\tBuilding using binutils"
     esp32ulp-elf-as -o $obj_file $src_file
     esp32ulp-elf-ld -T esp32.ulp.ld -o $elf_file $obj_file
     esp32ulp-elf-objcopy -O binary $elf_file $bin_file
 
-    if ! diff $ulp_file $bin_file; then
+    if ! diff $ulp_file $bin_file 1>/dev/null; then
+        echo -e "\tBuild outputs differ!"
+        echo ""
         echo "Compatibility test failed for $src_file"
+        echo "py-esp32-ulp log:"
+        cat $log_file
         echo "py-esp32-ulp output:"
-        hexdump $ulp_file
+        xxd -e $ulp_file
         echo "binutils output:"
-        hexdump $bin_file
+        xxd -e $bin_file
         exit 1
     else
-        echo "Build outputs match for $src_file"
+        echo -e "\tBuild outputs match"
     fi
 done
