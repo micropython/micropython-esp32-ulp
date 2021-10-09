118bdab75dbf4e530538d7557615081bc3f93f47
Wilko Nienhaus <wilko.nienhaus@gmail.com>
2021-10-09 09:29:30 +0300

    make imports less ugly
    If code outside our main entry point, e.g. our examples, wanted
    to use the src_to_binary function, it needed an ugly import of
    esp32_ulp.__main__. With this change the import becomes cleaner,
    e.g. `from esp32_ulp import src_to_binary`.
    
    The original "main" method, which took a filename as input and
    resulted in a ULP binary file as output, is now also more cleanly
    available as assemble_file() inside the esp32_ulp module.
    
    Fixes #39.

--- esp32_ulp/__main__.py
+++ esp32_ulp/__main__.py
@@ -1,36 +1,9 @@
 import sys
-
-from .util import garbage_collect
-
-from .preprocess import preprocess
-from .assemble import Assembler
-from .link import make_binary
-garbage_collect('after import')
-
-
-def src_to_binary(src):
-    assembler = Assembler()
-    src = preprocess(src)
-    assembler.assemble(src, remove_comments=False)  # comments already removed by preprocessor
-    garbage_collect('before symbols export')
-    addrs_syms = assembler.symbols.export()
-    for addr, sym in addrs_syms:
-        print('%04d %s' % (addr, sym))
-
-    text, data, bss_len = assembler.fetch()
-    return make_binary(text, data, bss_len)
+from . import assemble_file
 
 
 def main(fn):
-    with open(fn) as f:
-        src = f.read()
-
-    binary = src_to_binary(src)
-
-    if fn.endswith('.s') or fn.endswith('.S'):
-        fn = fn[:-2]
-    with open(fn + '.ulp', 'wb') as f:
-        f.write(binary)
+    assemble_file(fn)
 
 
 if __name__ == '__main__':
