8e2dde13a6691d92d7eaa89c324daf7b6353b685
Wilko Nienhaus <53194724+wnienhaus@users.noreply.github.com>
2021-10-14 16:45:36 +0300

    Make upip installable (#56)
    add setup.py to build upip installable distribution package for pypi
    
    setup.py (setuptools) uses the sdist_upip module (included in this
    commit) to generate packages in the correct format for use with upip.
    
    To generate a distribution package, make sure the VERSION is set as
    desired in setup.py and then run `python3 setup.py sdist`. The result
    will be a tar.gz file in the dist/ subdirectory.
    
    also: add additional supported platforms
    
    It appears the "platforms" fields it not actually used by any tools but
    can be used to communicate to users what platforms the package is meant
    to work on. (https://stackoverflow.com/a/34994245)
    
    Including linux and darwin here, because the code runs well on the unix
    port of MicroPython, which itself runs well on Linux and macOS (darwin).
    In fact most of the testing had been done using the unix port.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from setuptools import setup
+import sdist_upip
+
+
+HERE = Path(__file__).parent
+README = (HERE / 'README.rst').read_text()
+
+VERSION = "1.0.0"
+
+setup(
+    name="micropython-py-esp32-ulp",
+    version=VERSION,
+    description="Assembler toolchain for the ESP32 ULP co-processor, written in MicroPython",
+    long_description=README,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/ThomasWaldmann/py-esp32-ulp",
+    license="MIT",
+    author="py-esp32-ulp authors",
+    author_email="tw@waldmann-edv.de",
+    maintainer="py-esp32-ulp authors",
+    maintainer_email="tw@waldmann-edv.de",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+    ],
+    platforms=["esp32", "linux", "darwin"],
+    cmdclass={"sdist": sdist_upip.sdist},
+    packages=["esp32_ulp"],
+)
